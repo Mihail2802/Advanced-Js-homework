02fd36b7b94016cd0c8caec600dc174cb4eacf16
Mihail2802 <mihailkuzmanovski@live.com>
2023-01-20 14:34:37 +0100

    homework 4 done

---
+++ homework-04 Jquery calls/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  let submitBtn = document.getElementById("submitBtn");
+  let resultTitle = document.getElementById("resultTitle");
+  let height = document.getElementById("height");
+  let weight = document.getElementById("weight");
+  let eyeColor = document.getElementById("eyeColor");
+  let hairColor = document.getElementById("hairColor");
+  submitBtn.addEventListener("click", function () {
+    $.ajax({
+      url: "https://swapi.dev/api/people/1",
+      contentType : "application/json" ,
+      type : "get", 
+      success: function (response) {
+        let responseObject = response
+        resultTitle.innerText = responseObject.name;
+        height.innerText = responseObject.height;
+        weight.innerText = responseObject.mass;
+        eyeColor.innerText = responseObject.eye_color;
+        hairColor.innerText = responseObject.hair_color;
+
+        console.log("success")
+      },
+      error: function (error) {
+        console.log(error);
+      },
+    });
+  });
+});
